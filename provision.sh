1e2b32f05997a90fab552695c288494c3e62f152
Marco Bakera <bakera@tbs1.de>
2018-08-21 20:18:09 +0200

    added pip packages.

--- provision.sh
+++ provision.sh
@@ -4,6 +4,9 @@ BREW_PKGS="ansible bash-completion mariadb htop jupyter"
 BREW_CASK_PKGS="disk-inventory-x skype-for-business the-unarchiver\
  transmission geogebra java libreoffice mu-editor wch-ch34x-usb-serial-driver \
  x2goclient xquartz google-chrome etcher"
+PIP_PKGS="mycli webuntis-cli bottle pygame"
 
 brew install $BREW_PKGS
 brew cask install $BREW_CASK_PKGS
+
+python3 -m pip install $PIP_PKGS
