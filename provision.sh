912ccc0f1348c9ba78ce0ba8d0df51cb42e4c36c
Marco Bakera <bakera@tbs1.de>
2018-08-22 21:00:08 +0200

    added umlet

--- provision.sh
+++ provision.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-BREW_PKGS="ansible bash-completion mariadb htop jupyter"
+BREW_PKGS="ansible bash-completion mariadb htop jupyter umlet"
 BREW_CASK_PKGS="disk-inventory-x skype-for-business the-unarchiver\
  transmission geogebra java libreoffice mu-editor wch-ch34x-usb-serial-driver \
  x2goclient xquartz google-chrome etcher"
