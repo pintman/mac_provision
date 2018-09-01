b0a8550f3558517453281e84c7dcec4bf3f74d13
Marco Bakera <bakera@tbs1.de>
2018-09-01 20:21:38 +0200

    added torbrowser, pibakery.

--- provision.sh
+++ provision.sh
@@ -3,7 +3,7 @@
 BREW_PKGS="ansible bash-completion mariadb nmap htop jupyter umlet"
 BREW_CASK_PKGS="disk-inventory-x gimp skype-for-business the-unarchiver\
  transmission geogebra java libreoffice mu-editor wch-ch34x-usb-serial-driver \
- x2goclient xquartz google-chrome etcher"
+ x2goclient xquartz google-chrome etcher torbrowser pibakery"
 PIP_PKGS="mycli webuntis-cli bottle pygame"
 
 brew install $BREW_PKGS
