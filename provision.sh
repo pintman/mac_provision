c2743baaf141155f0d095e078f97ad4185eb57cd
Marco Bakera <bakera@tbs1.de>
2018-09-03 14:53:20 +0200

    added mycli.

--- provision.sh
+++ provision.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-BREW_PKGS="ansible bash-completion mariadb nmap htop jupyter umlet"
+BREW_PKGS="ansible bash-completion mariadb mycli nmap htop jupyter umlet"
 BREW_CASK_PKGS="disk-inventory-x gimp skype-for-business the-unarchiver\
  transmission geogebra java libreoffice mu-editor wch-ch34x-usb-serial-driver \
  x2goclient xquartz google-chrome etcher torbrowser pibakery"
