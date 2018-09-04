cd7496e6842b0e80cae582bfe2ec0910abec310e
Marco Bakera <bakera@tbs1.de>
2018-09-04 15:02:57 +0200

    added gpg-suite.

--- provision.sh
+++ provision.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 BREW_PKGS="ansible bash-completion mariadb mycli nmap htop jupyter umlet"
-BREW_CASK_PKGS="disk-inventory-x gimp skype-for-business the-unarchiver\
- transmission geogebra java libreoffice mu-editor wch-ch34x-usb-serial-driver \
+BREW_CASK_PKGS="disk-inventory-x gimp gpg-suite skype-for-business \
+ the-unarchiver \
+ transmission geogebra java libreoffice mu-editor \
+ wch-ch34x-usb-serial-driver \
  x2goclient xquartz google-chrome etcher torbrowser pibakery"
 PIP_PKGS="mycli webuntis-cli bottle pygame"
 
