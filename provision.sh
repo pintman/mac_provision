f78ea82837f8991a6e32e07efa9f0ecf6028d63a
Marco Bakera <bakera@tbs1.de>
2018-09-14 18:38:00 +0200

    added mypy and pylint.

--- provision.sh
+++ provision.sh
@@ -7,7 +7,7 @@ BREW_CASK_PKGS="disk-inventory-x gimp gpg-suite skype-for-business \
  transmission geogebra java libreoffice mu-editor \
  wch-ch34x-usb-serial-driver \
  x2goclient xquartz google-chrome etcher torbrowser pibakery"
-PIP_PKGS="mycli webuntis-cli bottle pygame"
+PIP_PKGS="mycli mypy pylint webuntis-cli bottle pygame"
 
 brew install $BREW_PKGS
 brew cask install $BREW_CASK_PKGS
