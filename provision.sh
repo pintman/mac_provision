bd42e7745bac96cbfcf87ca3baffcf78447524f5
Marco Bakera <bakera@tbs1.de>
2020-07-19 22:21:14 +0200

    feat: add barrier cask

--- provision.sh
+++ provision.sh
@@ -3,6 +3,7 @@
 BREW_PKGS="ansible bash-completion mariadb mycli nmap plantuml \
   htop jupyter umlet"
 BREW_CASK_PKGS="anki disk-inventory-x gimp gpg-suite keepassxc \
+ barrier \
  mactex texstudio \
  scribus ghostscript \
  the-unarchiver \
