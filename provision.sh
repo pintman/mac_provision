be26f663588916e1dcf0a30ddbedd0573381c58b
Marco Bakera <pintman@users.noreply.github.com>
2021-02-07 18:29:14 +0100

    updated packages

--- provision.sh
+++ provision.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-BREW_PKGS="ansible bash-completion mariadb mycli nmap plantuml \
+BREW_PKGS="ansible bash-completion mycli nmap plantuml \
   htop jupyter umlet"
 BREW_CASK_PKGS="anki disk-inventory-x gimp gpg-suite keepassxc \
  barrier \
  mactex texstudio \
- scribus ghostscript \
  the-unarchiver \
  transmission geogebra java libreoffice mu-editor \
  wch-ch34x-usb-serial-driver \
  veracrypt \
  visual-studio-code \
- x2goclient xquartz google-chrome etcher torbrowser wireshark virtualbox"
+ xquartz google-chrome etcher torbrowser wireshark virtualbox"
 PIP_PKGS="mypy pylint webuntis-cli bottle pygame"
 
 brew install $BREW_PKGS
